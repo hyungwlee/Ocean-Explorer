a0967f7fbfeebd4ac51212e4c6a94dcb040b4835
Haseeb Garfinkel <haseebgarfinkel@gmail.com>
2024-10-27 17:22:56 -0700

    Initial Commit

---
+++ Shared/Ocean_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ocean_ExplorerApp.swift
+//  Shared
+//
+//  Created by Haseeb Garfinkel on 10/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Ocean_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

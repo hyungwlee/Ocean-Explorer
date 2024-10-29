eb365577a6c58f6bef9bbef23f9a919faf0ce510
HaseebGarfinkel <haseebgarfinkel@gmail.com>
2024-10-28 20:36:12 -0700

    Created helper files

---
+++ Code/Helpers/OELayoutInfo.swift
@@ -0,0 +1,13 @@
+//
+//  OELayoutInfo.swift
+//  Ocean Explorer (iOS)
+//
+//  Created by Haseeb Garfinkel on 10/28/24.
+//
+
+import Foundation
+
+struct OELayoutInfo {
+    let screenSize: CGSize
+    let boxSize: CGSize = .init(width: 100, height: 100)
+}

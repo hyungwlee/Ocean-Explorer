e88fda2c53ef3d48458f4b7e9d905a9b2fa6c43a
Alexander Chakmakian <>
2024-11-01 11:00:44 -0700

    Setup the background where it generates as u go up

--- Code/OEGameScene.swift
+++ Code/OEGameScene.swift
@@ -4,18 +4,26 @@
 //
 //  Created by Alexander Chakmakian on 10/30/24.
 //
+
 import SpriteKit
 
 class OEGameScene: SKScene {
     weak var context: OEGameContext?
     var box: OEBoxNode?
     var cameraNode: SKCameraNode!
+    
+    // Array to track background tiles
+    var backgroundTiles: [SKSpriteNode] = []
 
     init(context: OEGameContext, size: CGSize) {
         self.context = context
         super.init(size: size)
-        self.cameraNode = SKCameraNode() // Initialize the camera node
-        self.camera = cameraNode // Set the scene's camera to this camera node
+        
+        // Center the scene’s anchor point
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        
+        self.cameraNode = SKCameraNode()
+        self.camera = cameraNode
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -29,19 +37,28 @@ class OEGameScene: SKScene {
         prepareGameContext()
         prepareStartNodes()
         
+        // Center the camera initially on the scene
+        cameraNode.position = CGPoint(x: 0, y: 0)
+        
         context.stateMachine?.enter(OEGameIdleState.self)
     }
     
     func setupBackground() {
-        // Set up the background image, scaled to the scene size
-        let backgroundNode = SKSpriteNode(imageNamed: "Background")
-        backgroundNode.size = size
-        backgroundNode.position = CGPoint(x: size.width / 2, y: size.height / 2)
-        backgroundNode.zPosition = -1
-        addChild(backgroundNode)
+        // Create the initial background tile
+        addBackgroundTile(at: CGPoint(x: 0, y: 0))
         
         // Add the camera to the scene
         addChild(cameraNode)
+    }
+
+    // Helper function to add a new background tile
+    func addBackgroundTile(at position: CGPoint) {
+        let backgroundNode = SKSpriteNode(imageNamed: "Background")
+        backgroundNode.size = size
+        backgroundNode.position = position
+        backgroundNode.zPosition = -1
+        addChild(backgroundNode)
+        backgroundTiles.append(backgroundNode)
     }
     
     func prepareGameContext() {
@@ -52,7 +69,7 @@ class OEGameScene: SKScene {
     }
     
     func prepareStartNodes() {
-        let center = CGPoint(x: size.width / 2, y: size.height / 2)
+        let center = CGPoint(x: 0, y: 0) // Centered in the scene
         box = OEBoxNode()
         box?.position = center
         if let box = box {
@@ -61,19 +78,40 @@ class OEGameScene: SKScene {
     }
     
     override func update(_ currentTime: TimeInterval) {
-        // Update camera position to follow the character as it moves up
         followCharacter()
+        updateBackgroundTiles()
+        //generateObstacles()
     }
     
     func followCharacter() {
-        // Ensure the camera follows the character's position, clamping if necessary
         if let box = box {
-            // Adjust the camera's y position based on the character's y position
             cameraNode.position.y = box.position.y
+        }
+    }
+
+    // Function to update background tiles as the player progresses
+    func updateBackgroundTiles() {
+        guard let box else { return }
+        
+        // Define when to add new tiles (adjust as needed)
+        let thresholdY = cameraNode.position.y + size.height / 2
+        
+        // Add a new tile if the last one is too far behind
+        if let lastTile = backgroundTiles.last, lastTile.position.y < thresholdY {
+            addBackgroundTile(at: CGPoint(x: 0, y: lastTile.position.y + size.height))
+        }
+
+        // Remove background tiles that are off-screen
+        backgroundTiles = backgroundTiles.filter { tile in
+            if tile.position.y < cameraNode.position.y - size.height {
+                tile.removeFromParent()
+                return false
+            }
+            return true
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        box?.jump() // Make the character jump up on tap
+        box?.jump()
     }
 }

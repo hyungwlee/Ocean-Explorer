fdc7603a1040e2d6f73a53ebd35dbc452921ed23
Kaleb <kalebhoching@gmail.com>
2024-12-19 21:48:43 -0800

    Added eel death animation

---
+++ Code/Nodes/OEShockedNode.swift
@@ -0,0 +1,26 @@
+import Foundation
+import SpriteKit
+
+class OECoralNode: SKSpriteNode {
+    
+    init(size: CGSize) {
+        let texture = SKTexture(imageNamed: "Coral") // Initial coral texture
+        super.init(texture: texture, color: .clear, size: size)
+        
+        // Set transparency
+        self.alpha = 0.60 // Adjust value between 0.0 (fully transparent) and 1.0 (fully opaque)
+        
+        // Configure physics body
+        self.physicsBody = SKPhysicsBody(rectangleOf: size)
+        self.physicsBody?.isDynamic = false // Stationary
+        self.physicsBody?.categoryBitMask = PhysicsCategory.coral
+        self.physicsBody?.contactTestBitMask = PhysicsCategory.box
+        self.physicsBody?.collisionBitMask = PhysicsCategory.box
+        
+        self.zPosition = 0
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
